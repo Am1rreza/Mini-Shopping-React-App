8d0c08de78ff667cc01df18204814546881f9912
Amirreza <amirreza.m13825@gmail.com>
2023-01-08 23:11:05 +0330

    increment button added and its styles added too

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,10 +3,13 @@ import styles from "./Product.module.css";
 const Product = ({ product, onDelete }) => {
   return (
     <div className={styles.product}>
-      <p>Product Name : {product.title} Course</p>
-      <p>Product Price : {product.price}</p>
-      <span>{product.quantity}</span>
-      <button onClick={onDelete}>Delete</button>
+      <p>{product.title} Course</p>
+      <p>Price : {product.price}</p>
+      <span className={styles.value}>{product.quantity}</span>
+      <button className={`${styles.button} ${styles.inc}`}>Increment</button>
+      <button className={styles.button} onClick={onDelete}>
+        Delete
+      </button>
     </div>
   );
 };
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,9 +4,9 @@ import Product from "../Product/Product";
 const ProductList = () => {
   const [products, setProducts] = useState([
     { id: 1, title: "React", price: "99$", quantity: 1 },
-    { id: 2, title: "HTML & CSS", price: "19$", quantity: 1 },
+    { id: 2, title: "HTML & CSS", price: "19$", quantity: 2 },
     { id: 3, title: "Node JS", price: "79$", quantity: 1 },
-    { id: 4, title: "Tailwind", price: "39$", quantity: 1 },
+    { id: 4, title: "Tailwind", price: "39$", quantity: 3 },
   ]);
 
   // handlers
