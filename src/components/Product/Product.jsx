d12f8eee55ae0ee0d370bac46a3736c9192cdb94
Amirreza <amirreza.m13825@gmail.com>
2023-01-09 19:17:32 +0330

    trash icon added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,18 @@
 import styles from "./Product.module.css";
+import { BiTrash } from "react-icons/bi";
 
-const Product = ({ product, onDelete, onIncrement, onChange,onDecrement }) => {
+const Product = ({ product, onDelete, onIncrement, onChange, onDecrement }) => {
   return (
     <div className={styles.product}>
       <p>{product.title} Course</p>
       <p>{product.price}</p>
       <span className={styles.value}>{product.quantity}</span>
-      <input className={styles.input} type="text" onChange={onChange} value={product.title} />
+      <input
+        className={styles.input}
+        type="text"
+        onChange={onChange}
+        value={product.title}
+      />
       <button
         className={`${styles.button} ${styles.inc}`}
         onClick={onIncrement}
@@ -17,7 +23,7 @@ const Product = ({ product, onDelete, onIncrement, onChange,onDecrement }) => {
         className={`${styles.button} ${styles.inc}`}
         onClick={onDecrement}
       >
-        -
+        {product.quantity > 1 ? "-" : <BiTrash style={{color:"#d71717"}} />}
       </button>
       <button className={styles.button} onClick={onDelete}>
         Delete
