d260c21e8bbbe2236831ab7232220737d2075a8f
Amirreza <amirreza.m13825@gmail.com>
2023-01-14 22:22:16 +0330

    App component converted to functional component

--- src/App.js
+++ src/App.js
@@ -1,91 +1,78 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ProductList from "./components/ProductList/ProductList";
 import Navbar from "./components/Navbar/Navbar";
 import Wrapper from "./components/HOC/Wrapper";
-import CounterOne from "./components/Context/CounterOne";
-import CounterProvider from "./components/Context/CounterProvider";
-import CountReducer from "./components/Context/Reducer/CountRecuer";
-import ImprovedCountReducer from "./components/Context/Reducer/ImprovedCountReducer";
 
-class App extends Component {
-  state = {
-    products: [
-      { id: 1, title: "React", price: "99$", quantity: 1 },
-      { id: 2, title: "HTML & CSS", price: "19$", quantity: 1 },
-      { id: 3, title: "Node JS", price: "79$", quantity: 1 },
-      { id: 4, title: "Tailwind", price: "39$", quantity: 1 },
-    ],
-  };
+const App = () => {
+  const [products, setProducts] = useState([
+    { id: 1, title: "React", price: "99$", quantity: 1 },
+    { id: 2, title: "HTML & CSS", price: "19$", quantity: 1 },
+    { id: 3, title: "Node JS", price: "79$", quantity: 1 },
+    { id: 4, title: "Tailwind", price: "39$", quantity: 1 },
+  ]);
 
   // handlers
-  removeHandler = (id) => {
-    const filteredProducts = this.state.products.filter((p) => p.id !== id);
-    this.setState({ products: filteredProducts });
+  const removeHandler = (id) => {
+    const filteredProducts = products.filter((p) => p.id !== id);
+    setProducts(filteredProducts);
   };
 
-  incrementHandler = (id) => {
-    const index = this.state.products.findIndex((p) => p.id === id);
+  const incrementHandler = (id) => {
+    const index = products.findIndex((p) => p.id === id);
     // get all of the products
-    const products = [...this.state.products];
+    const allProducts = [...products];
     // clone the selected index and update the quantity
-    const product = { ...this.state.products[index] };
+    const product = { ...products[index] };
     product.quantity++;
     // replace the product
-    products[index] = product;
+    allProducts[index] = product;
     // setState
-    this.setState({ products });
+    setProducts(allProducts);
   };
 
-  changeHandler = (e, id) => {
-    const index = this.state.products.findIndex((p) => p.id === id);
+  const changeHandler = (e, id) => {
+    const index = products.findIndex((p) => p.id === id);
     // get all of the products
-    const products = [...this.state.products];
+    const allProducts = [...products];
     // clone the selected index and update the title
-    const product = { ...this.state.products[index] };
+    const product = { ...products[index] };
     product.title = e.target.value;
     // replace the product
-    products[index] = product;
+    allProducts[index] = product;
     // setState
-    this.setState({ products });
+    setProducts(allProducts);
   };
 
-  decrementHandler = (id) => {
-    const index = this.state.products.findIndex((p) => p.id === id);
+  const decrementHandler = (id) => {
+    const index = products.findIndex((p) => p.id === id);
     // get all of the products
-    const products = [...this.state.products];
+    const allProducts = [...products];
     // clone the selected index and update the quantity
-    const product = { ...this.state.products[index] };
+    const product = { ...products[index] };
     if (product.quantity === 1) {
-      this.removeHandler(id);
+      removeHandler(id);
       return;
     }
     product.quantity--;
     // replace the product
-    products[index] = product;
+    allProducts[index] = product;
     // setState
-    this.setState({ products });
+    setProducts(allProducts);
   };
 
-  render() {
-    return (
-      <>
-        {/* <Navbar
-          totalItems={this.state.products.filter((p) => p.quantity > 0).length}
-        />
-        <ProductList
-          products={this.state.products}
-          onRemove={this.removeHandler}
-          onChange={this.changeHandler}
-          onIncrement={this.incrementHandler}
-          onDecrement={this.decrementHandler}
-        /> */}
-        <CounterProvider>
-          <ImprovedCountReducer/>
-        </CounterProvider>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Navbar totalItems={products.filter((p) => p.quantity > 0).length} />
+      <ProductList
+        products={products}
+        onRemove={removeHandler}
+        onChange={changeHandler}
+        onIncrement={incrementHandler}
+        onDecrement={decrementHandler}
+      />
+    </>
+  );
+};
 
 export default Wrapper(App, "container");
