29e91fb7ac57f925c6579c327f802da63fca72ba
Amirreza <amirreza.m13825@gmail.com>
2023-01-14 22:56:02 +0330

    handler functions moved from App component to ProductList component

--- src/App.js
+++ src/App.js
@@ -3,75 +3,14 @@ import React from "react";
 import ProductList from "./components/ProductList/ProductList";
 import Navbar from "./components/Navbar/Navbar";
 import Wrapper from "./components/HOC/Wrapper";
-import ProductsProvider, {
-  useProducts,
-  useProductsAction,
-} from "./components/Providers/ProductsProvider";
+import ProductsProvider from "./components/Providers/ProductsProvider";
 
 const App = () => {
-  // import state
-  const products = useProducts();
-  const setProducts = useProductsAction();
-
-  // handlers
-  const removeHandler = (id) => {
-    const filteredProducts = products.filter((p) => p.id !== id);
-    setProducts(filteredProducts);
-  };
-
-  const incrementHandler = (id) => {
-    const index = products.findIndex((p) => p.id === id);
-    // get all of the products
-    const allProducts = [...products];
-    // clone the selected index and update the quantity
-    const product = { ...products[index] };
-    product.quantity++;
-    // replace the product
-    allProducts[index] = product;
-    // setState
-    setProducts(allProducts);
-  };
-
-  const changeHandler = (e, id) => {
-    const index = products.findIndex((p) => p.id === id);
-    // get all of the products
-    const allProducts = [...products];
-    // clone the selected index and update the title
-    const product = { ...products[index] };
-    product.title = e.target.value;
-    // replace the product
-    allProducts[index] = product;
-    // setState
-    setProducts(allProducts);
-  };
-
-  const decrementHandler = (id) => {
-    const index = products.findIndex((p) => p.id === id);
-    // get all of the products
-    const allProducts = [...products];
-    // clone the selected index and update the quantity
-    const product = { ...products[index] };
-    if (product.quantity === 1) {
-      removeHandler(id);
-      return;
-    }
-    product.quantity--;
-    // replace the product
-    allProducts[index] = product;
-    // setState
-    setProducts(allProducts);
-  };
-
   return (
     <>
       <ProductsProvider>
-        <Navbar/>
-        <ProductList
-          onRemove={removeHandler}
-          onChange={changeHandler}
-          onIncrement={incrementHandler}
-          onDecrement={decrementHandler}
-        />
+        <Navbar />
+        <ProductList />
       </ProductsProvider>
     </>
   );
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,15 +1,65 @@
 import Product from "../Product/Product";
-import { useProducts } from "../Providers/ProductsProvider";
+import { useProducts, useProductsAction } from "../Providers/ProductsProvider";
 
-const ProductList = ({ onRemove, onDecrement, onChange, onIncrement }) => {
+const ProductList = () => {
   const products = useProducts();
-  
+  const setProducts = useProductsAction();
+
   // conditional rendering
   if (products.length === 0) {
     return (
       <h2 style={{ marginTop: "0.5rem" }}>There is no product in your cart!</h2>
     );
   }
+
+  // handlers
+  const removeHandler = (id) => {
+    const filteredProducts = products.filter((p) => p.id !== id);
+    setProducts(filteredProducts);
+  };
+
+  const incrementHandler = (id) => {
+    const index = products.findIndex((p) => p.id === id);
+    // get all of the products
+    const allProducts = [...products];
+    // clone the selected index and update the quantity
+    const product = { ...products[index] };
+    product.quantity++;
+    // replace the product
+    allProducts[index] = product;
+    // setState
+    setProducts(allProducts);
+  };
+
+  const changeHandler = (e, id) => {
+    const index = products.findIndex((p) => p.id === id);
+    // get all of the products
+    const allProducts = [...products];
+    // clone the selected index and update the title
+    const product = { ...products[index] };
+    product.title = e.target.value;
+    // replace the product
+    allProducts[index] = product;
+    // setState
+    setProducts(allProducts);
+  };
+
+  const decrementHandler = (id) => {
+    const index = products.findIndex((p) => p.id === id);
+    // get all of the products
+    const allProducts = [...products];
+    // clone the selected index and update the quantity
+    const product = { ...products[index] };
+    if (product.quantity === 1) {
+      removeHandler(id);
+      return;
+    }
+    product.quantity--;
+    // replace the product
+    allProducts[index] = product;
+    // setState
+    setProducts(allProducts);
+  };
 
   return (
     <div>
@@ -18,10 +68,10 @@ const ProductList = ({ onRemove, onDecrement, onChange, onIncrement }) => {
           <Product
             key={product.id}
             product={product}
-            onDelete={() => onRemove(product.id)}
-            onChange={(e) => onChange(e, product.id)}
-            onIncrement={() => onIncrement(product.id)}
-            onDecrement={() => onDecrement(product.id)}
+            onDelete={() => removeHandler(product.id)}
+            onChange={(e) => changeHandler(e, product.id)}
+            onIncrement={() => incrementHandler(product.id)}
+            onDecrement={() => decrementHandler(product.id)}
           />
         );
       })}
